a15a3d02ee2c00b3ee1cf3ee69dd02837ca35978
AdnanAhmadMQ <adnan.ahmad@students.mq.edu.au>
2023-03-07 23:36:01 +1100

    inital commit, base to start on

--- App.js
+++ App.js
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
-
+import { Button } from 'react-native-web';
+//export ANDROID_SDK=C:\Users\adnan\AppData\Local\Android\Sdk
+//use npm start or npm run andrioid 
 export default function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text>Its working this isn't a warning gz u got it running gj</Text>
+      <StatusBar style="auto"/>
     </View>
   );
 }
@@ -16,5 +19,6 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: "red",
   },
 });
